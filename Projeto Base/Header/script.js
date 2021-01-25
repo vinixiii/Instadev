13584b5cf786c31c5a837697691150dcf3e1bcac
Vinícius <vinissfigueiroa@gmail.com>
2021-01-25 08:36:24 -0300

    JavaScript da barra de busca adicionado

---
+++ Projeto Base/Header/script.js
@@ -0,0 +1,9 @@
+var barraDeBusca = document.querySelector(".busca-container");
+
+document.addEventListener("click", function(event) {
+    if(event.target.closest("#busca")) {
+        barraDeBusca.classList.add("buscando");
+        return
+    }
+    barraDeBusca.classList.remove("buscando");
+})
