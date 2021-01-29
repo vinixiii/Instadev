376b0582161f5acf403ef249aaa222b51ad5cb5a
Taila <taila.franco@aluno.ufabc.edu.br>
2021-01-29 15:33:51 -0300

    Finalização da page-follower

--- Projeto Base/Perfil amigo/script.js
+++ Projeto Base/Perfil amigo/script.js
@@ -13,3 +13,19 @@ botao.addEventListener("click", function() {
 
     this.classList.toggle("btnfct");
 });
+
+let like = "unliked";
+
+function curtir(id) {
+    let heart = document.getElementById(id);
+    console.log(id);
+
+    if(like == "unliked") {
+        heart.src="./images/like-painted.svg";
+        like = "liked";
+        console.log(heart);
+    } else {
+        heart.src="./images/like.svg";
+        like = "unliked";
+    }
+};
