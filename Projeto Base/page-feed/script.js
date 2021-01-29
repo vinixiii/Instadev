2ac06de51f6e4dc66b007465add0ce5c0fcf80a3
Taila <taila.franco@aluno.ufabc.edu.br>
2021-01-29 14:21:59 -0300

    page-feed finalizada

--- Projeto Base/page-feed/script.js
+++ Projeto Base/page-feed/script.js
@@ -8,3 +8,18 @@ document.addEventListener("click", function(event) {
     barraDeBusca.classList.remove("buscando");
 })
 
+let like = "unliked";
+
+function mudarImagem(id) {
+    let heart = document.getElementById(id);
+    console.log(id);
+
+    if(like == "unliked") {
+        heart.src="./img/heart-2-pintado1.svg";
+        like = "liked";
+        console.log(heart);
+    } else {
+        heart.src="./img/Vectors2.svg";
+        like = "unliked";
+    }
+};
