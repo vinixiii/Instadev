3001901a2e6783d9d576f33b4472f8ddc8191ffb
Vinícius <vinissfigueiroa@gmail.com>
2021-02-04 10:38:39 -0300

    Barra de pesquisa finalizada

--- wwwroot/js/pesquisa.js
+++ wwwroot/js/pesquisa.js
@@ -17,6 +17,7 @@ let campoFiltro = document.querySelector("#buscar");
 campoFiltro.addEventListener("input", function() {
     console.log(this.value);
     let posts = document.querySelectorAll(".post-feed");
+    let stories = document.querySelectorAll(".friends");
 
     if (campoFiltro.value.length != 0){
         for (let i = 0; i < posts.length; i++) {
@@ -24,16 +25,35 @@ campoFiltro.addEventListener("input", function() {
             let postNome = post.querySelector(".nome-publicacao");
             let nome = postNome.textContent;
 
-            if(nome != this.value) {
+            var expressao = new RegExp(this.value, "i");
+            if(!expressao.test(nome)) {
                 post.classList.add("invisivel");
             } else {
                 post.classList.remove("invisivel");
+            }
+        }
+
+        for (let i = 0; i < stories.length; i++) {
+            let story = stories[i];
+            let storyNome = story.querySelector(".nome-stories");
+            let nome = storyNome.textContent;
+
+            if(!expressao.test(nome)) {
+                story.classList.add("invisivel");
+            } else {
+                story.classList.remove("invisivel");
             }
         }
     } else {
         for (let i = 0; i < posts.length; i++) {
             let post = posts[i];
             post.classList.remove("invisivel");
+            
+        }
+
+        for (let i = 0; i < stories.length; i++) {
+            let story = stories[i];
+            story.classList.remove("invisivel");
             
         }
     }
